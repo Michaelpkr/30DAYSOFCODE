c6cda89e4d4b11e1ceba9e4dc6e3cc4184d5f626
Michaelpkr <michael.parker@ramseysolutions.com>
2022-10-12 20:21:53 -0500

    Day 3 adds improved navigation and mobile styling

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function darkModeSwitch() {
     let element = document.body;
     element.classList.toggle("dark");
     }
+
+let currentPage = Number(document.querySelector("#pageIdentify").innerHTML)
+
+toPreviousLink = document.getElementById("toPrevious");
+toNextLink = document.getElementById("toNext");
+
+toPreviousLink.setAttribute("href", `day${currentPage - 1}.html`)
+toNextLink.setAttribute("href", `day${currentPage + 1}.html`)
