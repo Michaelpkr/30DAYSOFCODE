c814978692ba6af22ff034b3778c95023bb8702e
Michaelpkr <michael.parker@ramseysolutions.com>
2022-10-13 20:01:01 -0500

    Adds Dark Mode Aesthetic Improvements

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+localStorage.setItem(userName, "there")
+
+if(localStorage.getItem(userName) === "there"){
+    let person = prompt("Please enter your name")
+    localStorage.setItem(userName, person)
+}
+
+document.getElementById('greeting').innerHTML = `Hello ${person}`
+
 function darkModeSwitch() {
     let element = document.body;
     element.classList.toggle("dark");
+    
+
+    if(document.getElementById("darkModeButton").innerHTML === "☽"){
+        document.getElementById("darkModeButton").innerHTML = "☼";
+        
+    }else if (document.getElementById("darkModeButton").innerHTML = "☼"){
+        document.getElementById("darkModeButton").innerHTML = "☽"
+    }
+
+    
+    
     }
 
 let currentPage = Number(document.querySelector("#pageIdentify").innerHTML)
@@ -10,3 +30,5 @@ toNextLink = document.getElementById("toNext");
 
 toPreviousLink.setAttribute("href", `https://michaelpkr.github.io/30DAYSOFCODE/day${currentPage - 1}.html`)
 toNextLink.setAttribute("href", `https://michaelpkr.github.io/30DAYSOFCODE/day${currentPage + 1}.html`)
+
+console.log(document.getElementById("darkModeButton").innerHTML)
