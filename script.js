d5d57d8b2dec4959920769ce1c7b5f723ed6a3c1
Michaelpkr <michael.parker@ramseysolutions.com>
2022-10-15 22:36:09 -0500

    Adds color preferences to preference center

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// RUN FUNCTIONS
+
+window.onload=function(){
+    askName();
+    preferences();
+    }
+
+// PAGE IDENTIFICATION / NAVIGATION ========================================
+
 let currentPage = Number(document.querySelector("#pageIdentify").innerHTML)
 
 toPreviousLink = document.getElementById("toPrevious");
@@ -5,6 +14,8 @@ toNextLink = document.getElementById("toNext");
 
 toPreviousLink.setAttribute("href", `https://michaelpkr.github.io/30DAYSOFCODE/day${currentPage - 1}.html`)
 toNextLink.setAttribute("href", `https://michaelpkr.github.io/30DAYSOFCODE/day${currentPage + 1}.html`)
+
+// DARKMODE=======================================================================
 
 console.log(document.getElementById("darkModeButton").innerHTML)
 
@@ -22,6 +33,8 @@ function darkModeSwitch() {
     
     }
 
+    // NAME PERSONALIZATION ================================================
+
     function askName() {
         let username = localStorage.getItem('username');
         if (!username) {
@@ -33,10 +46,79 @@ function darkModeSwitch() {
         } else {
             document.getElementById("noUser").innerHTML = "Welcome, Stranger!";
         }
-      }  
+      }
 
-     function toggleMenu(){
-        let preferences = document.getElementById("preferencesMain")
-        let preferencesChild = document.getElementsByClassName("preferences");
-        preferencesChild.classList.add("hide")
-     }
+    //   PREFERENCES MODAL ====================================================
+
+            // Get the modal
+        var modal = document.getElementById("myModal");
+
+        // Get the button that opens the modal
+        var btn = document.getElementById("myBtn");
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks on the button, open the modal
+        btn.onclick = function() {
+        modal.style.display = "block";
+        }
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+        modal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+        }
+
+        // PREFERENCES
+
+        function preferences(){
+            let currentColor = localStorage.getItem("color")
+            document.getElementById("navManipulation").classList.add(currentColor)
+        }
+
+        let red = document.getElementById("red")
+        red.onclick = function(){
+        let currentColor = localStorage.getItem("color")
+        document.getElementById("navManipulation").classList.remove(`${currentColor}`)
+        document.getElementById("navManipulation").classList.add("red")
+        localStorage.setItem('color', "red");
+        }
+
+        let blue = document.getElementById("blue")
+        blue.onclick = function(){
+        let currentColor = localStorage.getItem("color")
+        document.getElementById("navManipulation").classList.remove(`${currentColor}`)
+        document.getElementById("navManipulation").classList.add("blue")
+        localStorage.setItem('color', "blue");
+        }
+
+        let yellow = document.getElementById("yellow")
+        yellow.onclick = function(){
+        let currentColor = localStorage.getItem("color")
+        document.getElementById("navManipulation").classList.remove(`${currentColor}`)
+        document.getElementById("navManipulation").classList.add("yellow")
+        localStorage.setItem('color', "yellow");
+        }
+
+        let green = document.getElementById("green")
+        green.onclick = function(){
+        let currentColor = localStorage.getItem("color")
+        document.getElementById("navManipulation").classList.remove(`${currentColor}`)
+        document.getElementById("navManipulation").classList.add("green")
+        localStorage.setItem('color', "green");
+        }
+
+        let black = document.getElementById("black")
+        black.onclick = function(){
+        let currentColor = localStorage.getItem("color")
+        document.getElementById("navManipulation").classList.remove(`${currentColor}`)
+        document.getElementById("navManipulation").classList.add("black")
+        localStorage.setItem('color', "black");
+        }
