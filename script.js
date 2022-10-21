4dc1ec33386dbf26ab91c08e910327ca1f4ab427
Michaelpkr <92385361+Michaelpkr@users.noreply.github.com>
2022-10-21 15:33:06 -0500

    Adds Pokemon Type/Weight/Height and improved mobile styling

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // RUN FUNCTIONS
 
-window.onload=function(){
+window.onload = function() {
     askName();
     preferences();
-    }
+}
 
 // PAGE IDENTIFICATION / NAVIGATION ========================================
 
@@ -22,166 +22,166 @@ console.log(document.getElementById("darkModeButton").innerHTML)
 function darkModeSwitch() {
     let element = document.body;
     element.classList.toggle("dark");
-    
 
-    if(document.getElementById("darkModeButton").innerHTML === "☽"){
+
+    if (document.getElementById("darkModeButton").innerHTML === "☽") {
         document.getElementById("darkModeButton").innerHTML = "☼";
-        
-    }else if (document.getElementById("darkModeButton").innerHTML = "☼"){
+
+    } else if (document.getElementById("darkModeButton").innerHTML = "☼") {
         document.getElementById("darkModeButton").innerHTML = "☽"
     }
-    
+
+}
+
+// NAME PERSONALIZATION ================================================
+
+function askName() {
+    let username = localStorage.getItem('username');
+    if (!username) {
+        username = prompt("Please enter your name:");
     }
+    if (username != null) {
+        document.getElementById("greeting").innerHTML = `Hey ${username},`
+        localStorage.setItem('username', username);
+    } else {
+        document.getElementById("noUser").innerHTML = "Welcome, Stranger!";
+    }
+}
 
-    // NAME PERSONALIZATION ================================================
+//   PREFERENCES MODAL ====================================================
 
-    function askName() {
-        let username = localStorage.getItem('username');
-        if (!username) {
-          username = prompt("Please enter your name:");
-        }
-        if (username != null) {
-            document.getElementById("greeting").innerHTML = `Hey ${username},`
-            localStorage.setItem('username', username);
-        } else {
-            document.getElementById("noUser").innerHTML = "Welcome, Stranger!";
-        }
-      }
+// Get the modal
+var modal = document.getElementById("myModal");
 
-    //   PREFERENCES MODAL ====================================================
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-            // Get the modal
-        var modal = document.getElementById("myModal");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-        // Get the button that opens the modal
-        var btn = document.getElementById("myBtn");
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
 
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
 
-        // When the user clicks on the button, open the modal
-        btn.onclick = function() {
-        modal.style.display = "block";
-        }
-
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
         modal.style.display = "none";
-        }
+    }
+}
 
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-        }
+// PREFERENCES
 
-        // PREFERENCES
+// BANNER COLOR SELECTOR
 
-        // BANNER COLOR SELECTOR
+function preferences() {
+    let currentColor = localStorage.getItem("color")
+    document.getElementById("navManipulation").classList.add(currentColor)
+    let currentTextColor = localStorage.getItem("currentTextColor")
+    document.getElementById("textManipulation").classList.add(currentTextColor)
+}
 
-        function preferences(){
-            let currentColor = localStorage.getItem("color")
-            document.getElementById("navManipulation").classList.add(currentColor)
-            let currentTextColor = localStorage.getItem("currentTextColor")
-            document.getElementById("textManipulation").classList.add(currentTextColor)
-        }
+let red = document.getElementById("red")
+red.onclick = function() {
+    let currentColor = localStorage.getItem("color")
+    document.getElementById("navManipulation").classList.remove(`${currentColor}`)
+    document.getElementById("navManipulation").classList.add("red")
+    localStorage.setItem('color', "red");
+}
 
-        let red = document.getElementById("red")
-        red.onclick = function(){
-        let currentColor = localStorage.getItem("color")
-        document.getElementById("navManipulation").classList.remove(`${currentColor}`)
-        document.getElementById("navManipulation").classList.add("red")
-        localStorage.setItem('color', "red");
-        }
+let blue = document.getElementById("blue")
+blue.onclick = function() {
+    let currentColor = localStorage.getItem("color")
+    document.getElementById("navManipulation").classList.remove(`${currentColor}`)
+    document.getElementById("navManipulation").classList.add("blue")
+    localStorage.setItem('color', "blue");
+}
 
-        let blue = document.getElementById("blue")
-        blue.onclick = function(){
-        let currentColor = localStorage.getItem("color")
-        document.getElementById("navManipulation").classList.remove(`${currentColor}`)
-        document.getElementById("navManipulation").classList.add("blue")
-        localStorage.setItem('color', "blue");
-        }
+let yellow = document.getElementById("yellow")
+yellow.onclick = function() {
+    let currentColor = localStorage.getItem("color")
+    document.getElementById("navManipulation").classList.remove(`${currentColor}`)
+    document.getElementById("navManipulation").classList.add("yellow")
+    localStorage.setItem('color', "yellow");
+}
 
-        let yellow = document.getElementById("yellow")
-        yellow.onclick = function(){
-        let currentColor = localStorage.getItem("color")
-        document.getElementById("navManipulation").classList.remove(`${currentColor}`)
-        document.getElementById("navManipulation").classList.add("yellow")
-        localStorage.setItem('color', "yellow");
-        }
+let green = document.getElementById("green")
+green.onclick = function() {
+    let currentColor = localStorage.getItem("color")
+    document.getElementById("navManipulation").classList.remove(`${currentColor}`)
+    document.getElementById("navManipulation").classList.add("green")
+    localStorage.setItem('color', "green");
+}
 
-        let green = document.getElementById("green")
-        green.onclick = function(){
-        let currentColor = localStorage.getItem("color")
-        document.getElementById("navManipulation").classList.remove(`${currentColor}`)
-        document.getElementById("navManipulation").classList.add("green")
-        localStorage.setItem('color', "green");
-        }
+let black = document.getElementById("black")
+black.onclick = function() {
+    let currentColor = localStorage.getItem("color")
+    document.getElementById("navManipulation").classList.remove(`${currentColor}`)
+    document.getElementById("navManipulation").classList.add("black")
+    localStorage.setItem('color', "black");
+}
 
-        let black = document.getElementById("black")
-        black.onclick = function(){
-        let currentColor = localStorage.getItem("color")
-        document.getElementById("navManipulation").classList.remove(`${currentColor}`)
-        document.getElementById("navManipulation").classList.add("black")
-        localStorage.setItem('color', "black");
-        }
+// TEXT COLOR SELECTOR
 
-        // TEXT COLOR SELECTOR
+let textred = document.getElementById("textred")
+textred.onclick = function() {
+    let currentTextColor = localStorage.getItem("currentTextColor")
+    document.getElementById("textManipulation").classList.remove(`${currentTextColor}`)
+    document.getElementById("textManipulation").classList.add("textred")
+    localStorage.setItem('currentTextColor', "textred");
+}
 
-        let textred = document.getElementById("textred")
-        textred.onclick = function(){
-        let currentTextColor = localStorage.getItem("currentTextColor")
-        document.getElementById("textManipulation").classList.remove(`${currentTextColor}`)
-        document.getElementById("textManipulation").classList.add("textred")
-        localStorage.setItem('currentTextColor', "textred");
-        }
+let textblue = document.getElementById("textblue")
+textblue.onclick = function() {
+    let currentTextColor = localStorage.getItem("currentTextColor")
+    document.getElementById("textManipulation").classList.remove(`${currentTextColor}`)
+    document.getElementById("textManipulation").classList.add("textblue")
+    localStorage.setItem('currentTextColor', "textblue");
+}
 
-        let textblue = document.getElementById("textblue")
-        textblue.onclick = function(){
-        let currentTextColor = localStorage.getItem("currentTextColor")
-        document.getElementById("textManipulation").classList.remove(`${currentTextColor}`)
-        document.getElementById("textManipulation").classList.add("textblue")
-        localStorage.setItem('currentTextColor', "textblue");
-        }
-        
-        let textyellow = document.getElementById("textyellow")
-        textyellow.onclick = function(){
-        let currentTextColor = localStorage.getItem("currentTextColor")
-        document.getElementById("textManipulation").classList.remove(`${currentTextColor}`)
-        document.getElementById("textManipulation").classList.add("textyellow")
-        localStorage.setItem('currentTextColor', "textyellow");
-        }
+let textyellow = document.getElementById("textyellow")
+textyellow.onclick = function() {
+    let currentTextColor = localStorage.getItem("currentTextColor")
+    document.getElementById("textManipulation").classList.remove(`${currentTextColor}`)
+    document.getElementById("textManipulation").classList.add("textyellow")
+    localStorage.setItem('currentTextColor', "textyellow");
+}
 
-        let textgreen = document.getElementById("textgreen")
-        textgreen.onclick = function(){
-        let currentTextColor = localStorage.getItem("currentTextColor")
-        document.getElementById("textManipulation").classList.remove(`${currentTextColor}`)
-        document.getElementById("textManipulation").classList.add("textgreen")
-        localStorage.setItem('currentTextColor', "textgreen");
-        }
-        
-        let textblack = document.getElementById("textblack")
-        textblack.onclick = function(){
-        let currentTextColor = localStorage.getItem("currentTextColor")
-        document.getElementById("textManipulation").classList.remove(`${currentTextColor}`)
-        document.getElementById("textManipulation").classList.add("textblack")
-        localStorage.setItem('currentTextColor', "textblack");
-        }
+let textgreen = document.getElementById("textgreen")
+textgreen.onclick = function() {
+    let currentTextColor = localStorage.getItem("currentTextColor")
+    document.getElementById("textManipulation").classList.remove(`${currentTextColor}`)
+    document.getElementById("textManipulation").classList.add("textgreen")
+    localStorage.setItem('currentTextColor', "textgreen");
+}
 
-        let textwhite = document.getElementById("textwhite")
-        textwhite.onclick = function(){
-        let currentTextColor = localStorage.getItem("currentTextColor")
-        document.getElementById("textManipulation").classList.remove(`${currentTextColor}`)
-        document.getElementById("textManipulation").classList.add("textwhite")
-        localStorage.setItem('currentTextColor', "textwhite");
-        }
+let textblack = document.getElementById("textblack")
+textblack.onclick = function() {
+    let currentTextColor = localStorage.getItem("currentTextColor")
+    document.getElementById("textManipulation").classList.remove(`${currentTextColor}`)
+    document.getElementById("textManipulation").classList.add("textblack")
+    localStorage.setItem('currentTextColor', "textblack");
+}
 
-        // APPLICATIONS
+let textwhite = document.getElementById("textwhite")
+textwhite.onclick = function() {
+    let currentTextColor = localStorage.getItem("currentTextColor")
+    document.getElementById("textManipulation").classList.remove(`${currentTextColor}`)
+    document.getElementById("textManipulation").classList.add("textwhite")
+    localStorage.setItem('currentTextColor', "textwhite");
+}
 
-        document.getElementById("getPokemon").addEventListener("click", getPokemon)
+// APPLICATIONS
 
-        document.getElementById("getRandomPokemon").addEventListener("click", getRandomPokemon)
+document.getElementById("getPokemon").addEventListener("click", getPokemon)
+
+document.getElementById("getRandomPokemon").addEventListener("click", getRandomPokemon)
 
 
 function getPokemon() {
@@ -194,6 +194,9 @@ function getPokemon() {
         .then(data => {
             console.log(data)
             document.getElementById("pokemonname").innerText = (data.species.name)
+            document.getElementById("pokemontype").innerText = `Pokemon Type: ${(data.types[0].type.name)}`
+            document.getElementById("pokemonheight").innerText = `Height: ${(data.height)}`
+            document.getElementById("pokemonweight").innerText = `Height: ${(data.weight)}`
             document.querySelector("img").src = (data.sprites.front_default)
             document.querySelector("h3").innerText = (data.height)
         })
@@ -212,6 +215,9 @@ function getRandomPokemon() {
         .then(data => {
             console.log(data)
             document.getElementById("pokemonname").innerText = (data.species.name)
+            document.getElementById("pokemontype").innerText = `Type: ${(data.types[0].type.name)}`
+            document.getElementById("pokemonheight").innerText = `Height: ${(data.height)}`
+            document.getElementById("pokemonweight").innerText = `Height: ${(data.weight)}`
             document.querySelector("img").src = (data.sprites.front_default)
             document.querySelector("h3").innerText = (data.height)
         })
